b58e3f99e80dc4cb803e8c7bd108421b501ad141
Hamed <mibarim.ir@gmail.com>
2017-12-25 19:59:25 +0330

    change passModel

--- app/src/main/java/com/mibarim/driver/models/ContactPassengersModel.java
+++ app/src/main/java/com/mibarim/driver/models/ContactPassengersModel.java
@@ -2,11 +2,13 @@ package com.mibarim.driver.models;
 
 import com.mibarim.driver.models.enums.PayingMethod;
 
+import java.io.Serializable;
+
 /**
  * Created by Alireza on 12/12/2017.
  */
 
-public class ContactPassengersModel {
+public class ContactPassengersModel implements Serializable {
     String ImageId;
     String PassengerName;
     PayingMethod PayingMethod;
